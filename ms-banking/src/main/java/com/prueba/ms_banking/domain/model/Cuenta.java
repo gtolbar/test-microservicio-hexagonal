7c6c5683b8124f2f94f5f52256afdb1659790a1f
Gustavo Toledo <toledo970501@gmail.com>
2025-02-28 20:26:32 -0500

    se add modelos

--- ms-banking/src/main/java/com/prueba/ms_banking/domain/model/Cuenta.java
+++ ms-banking/src/main/java/com/prueba/ms_banking/domain/model/Cuenta.java
@@ -1,1 +1,68 @@
-public class Cuenta {}
+package com.prueba.ms_banking.domain.model;
+
+public class Cuenta {
+
+    private Long id;
+    private String numeroCuenta;
+    private String tipoCuenta; // Ahorro, Corriente, etc.
+    private Double saldoInicial;
+    private Boolean estado;
+    private Long clienteId; // ID del cliente asociado
+
+    public Cuenta(Long id, String numeroCuenta, String tipoCuenta, Double saldoInicial, Boolean estado, Long clienteId) {
+        this.id = id;
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.saldoInicial = saldoInicial;
+        this.estado = estado;
+        this.clienteId = clienteId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getTipoCuenta() {
+        return tipoCuenta;
+    }
+
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public Double getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(Double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+}
--- ms-banking/src/main/java/com/prueba/ms_banking/domain/model/Movimiento.java
+++ ms-banking/src/main/java/com/prueba/ms_banking/domain/model/Movimiento.java
@@ -1,1 +1,70 @@
-public class Movimiento {}
+package com.prueba.ms_banking.domain.model;
+
+import java.time.LocalDateTime;
+
+public class Movimiento {
+
+    private Long id;
+    private LocalDateTime fecha;
+    private String tipoMovimiento; // Retiro, Depósito
+    private Double valor;
+    private Double saldoDisponible;
+    private String numeroCuenta; // Número de cuenta asociada
+
+    public Movimiento(Long id, LocalDateTime fecha, String tipoMovimiento, Double valor, Double saldoDisponible, String numeroCuenta) {
+        this.id = id;
+        this.fecha = fecha;
+        this.tipoMovimiento = tipoMovimiento;
+        this.valor = valor;
+        this.saldoDisponible = saldoDisponible;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Double getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(Double saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+}
