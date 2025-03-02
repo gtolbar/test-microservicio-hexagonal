66f152c37a6c57ca7bcd50818c61c4e29f9c2346
Gustavo Toledo <toledo970501@gmail.com>
2025-03-01 21:05:19 -0500

    se configura

--- ms-identity/src/main/java/com/prueba/ms_identity/application/dto/ClienteRequest.java
+++ ms-identity/src/main/java/com/prueba/ms_identity/application/dto/ClienteRequest.java
@@ -1,5 +1,6 @@
 package com.prueba.ms_identity.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class ClienteRequest {
 
+    @JsonIgnore
     private Long id;
     private String nombre;
     private String genero;
--- ms-identity/src/main/java/com/prueba/ms_identity/infraestructure/input/rest/ClienteController.java
+++ ms-identity/src/main/java/com/prueba/ms_identity/infraestructure/input/rest/ClienteController.java
@@ -1,4 +1,50 @@
 package com.prueba.ms_identity.infraestructure.input.rest;
 
+import com.prueba.ms_identity.application.dto.ClienteRequest;
+import com.prueba.ms_identity.application.dto.ClienteResponse;
+import com.prueba.ms_identity.application.handler.IClienteHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final IClienteHandler handler;
+
+    public ClienteController(IClienteHandler handler) {
+        this.handler = handler;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> saveIn(@RequestBody ClienteRequest request){
+        handler.saveModelIn(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<ClienteResponse>> getAllFrom(){
+        return ResponseEntity.ok(handler.getAllModelFrom());
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<ClienteResponse> getFrom(@PathVariable(name = "number") Long id){
+        return ResponseEntity.ok(handler.getModelFrom(id));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> updateIn(@RequestBody ClienteRequest request){
+        handler.updateModelIn(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{number}")
+    public ResponseEntity<Void> deletesIn(@PathVariable(name = "number") Long id){
+        handler.deleteModelFrom(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
