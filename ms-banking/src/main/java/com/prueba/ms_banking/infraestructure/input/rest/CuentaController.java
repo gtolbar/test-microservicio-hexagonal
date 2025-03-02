0ff6c95c5109900ed072e85e143535c3ed2e6612
Gustavo Toledo <toledo970501@gmail.com>
2025-03-02 01:35:05 -0500

    se ajusta mapstructs

--- ms-banking/src/main/java/com/prueba/ms_banking/infraestructure/input/rest/CuentaController.java
+++ ms-banking/src/main/java/com/prueba/ms_banking/infraestructure/input/rest/CuentaController.java
@@ -1,3 +1,51 @@
 package com.prueba.ms_banking.infraestructure.input.rest;
 
-public class CuentaController {}
+import com.prueba.ms_banking.application.dto.CuentaRequest;
+import com.prueba.ms_banking.application.dto.CuentaResponse;
+import com.prueba.ms_banking.application.handler.ICuentaHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cuentas")
+public class CuentaController {
+
+    private final ICuentaHandler handler;
+
+    public CuentaController(ICuentaHandler handler) {
+        this.handler = handler;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> saveIn(@RequestBody CuentaRequest request){
+        handler.saveModelIn(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<CuentaResponse>> getAllFrom(){
+        return ResponseEntity.ok(handler.getAllModelFrom());
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<CuentaResponse> getFrom(@PathVariable(name = "number") Long id){
+        return ResponseEntity.ok(handler.getModelFrom(id));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> updateIn(@RequestBody CuentaRequest request){
+        handler.updateModelIn(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{number}")
+    public ResponseEntity<Void> deletesIn(@PathVariable(name = "number") Long id){
+        handler.deleteModelFrom(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
--- ms-banking/src/main/java/com/prueba/ms_banking/infraestructure/input/rest/MovimientoController.java
+++ ms-banking/src/main/java/com/prueba/ms_banking/infraestructure/input/rest/MovimientoController.java
@@ -1,3 +1,50 @@
 package com.prueba.ms_banking.infraestructure.input.rest;
 
-public class MovimientoController {}
+import com.prueba.ms_banking.application.dto.MovimientoRequest;
+import com.prueba.ms_banking.application.dto.MovimientoResponse;
+import com.prueba.ms_banking.application.handler.IMovimientoHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movimientos")
+public class MovimientoController {
+
+    private final IMovimientoHandler handler;
+
+    public MovimientoController(IMovimientoHandler handler) {
+        this.handler = handler;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Void> saveIn(@RequestBody MovimientoRequest request){
+        handler.saveModelIn(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<MovimientoResponse>> getAllFrom(){
+        return ResponseEntity.ok(handler.getAllModelFrom());
+    }
+
+    @GetMapping("/{number}")
+    public ResponseEntity<MovimientoResponse> getFrom(@PathVariable(name = "number") Long id){
+        return ResponseEntity.ok(handler.getModelFrom(id));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Void> updateIn(@RequestBody MovimientoRequest request){
+        handler.updateModelIn(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{number}")
+    public ResponseEntity<Void> deletesIn(@PathVariable(name = "number") Long id){
+        handler.deleteModelFrom(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
